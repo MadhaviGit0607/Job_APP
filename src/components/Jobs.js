81bf39402215b38669a084f2814975a9e56107f0
MadhaviGit0607 <162568991+MadhaviGit0607@users.noreply.github.com>
2024-09-12 15:28:02 +0530

    Update Jobs.js

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Jobs.css'; 
-// Sample API URL and page size
+
 const API_URL = 'https://testapi.getlokalapp.com/common/jobs?page=1';
 
 
