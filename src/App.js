2605183932b8eceb957cab990c49780aa900a30b
MadhaviGit0607 <162568991+MadhaviGit0607@users.noreply.github.com>
2024-09-13 18:04:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Jobs from './components/Jobs';
 import Bookmarks from './components/Bookmarks';
 import JobDetails from './components/JobDetails';
-import './App.css
+import './App.css'
 
 const getBookmarksFromStorage = () => {
   const storedBookmarks = localStorage.getItem('bookmarkedJobs');
